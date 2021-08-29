2b7b84d4fa9bc4fe57fa3c328987d9a969ef4e37
unknown <tw450497@gmail.com>
2021-08-29 17:02:10 +0900

    Add: /test/hahaha 테스트

--- HelloExpress/routes/test.js
+++ HelloExpress/routes/test.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('test');
 });
 
+router.get("/hahaha", function(req, res, next){
+  res.send("새로운 라우터가 등록되었습니다.")
+})
+
+
 module.exports = router;
