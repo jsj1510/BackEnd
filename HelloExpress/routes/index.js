2d359a6c005e2875d72086d7e50ad1a60307c2bf
unknown <tw450497@gmail.com>
2021-08-29 17:10:22 +0900

    include 설정 ex) header or footer 추가

--- HelloExpress/routes/index.js
+++ HelloExpress/routes/index.js
@@ -2,14 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
 router.get('/', function(req, res, next) {
-  res.render('product/edit', {
-    title: "Express"
-  });
+  res.render('index', { title: 'Express' });
 });
+
+// depth 경로 설정
+// router.get('/', function(req, res, next) {
+//   res.render('product/edit', {
+//     title: "Express"
+//   });
+// });
 
 module.exports = router;
