7742a49de9564baba7db0f92027e0ad088dd8051
unknown <tw450497@gmail.com>
2021-08-29 18:37:53 +0900

    db 연결설정 실패 404error

--- HelloExpress/routes/index.js
+++ HelloExpress/routes/index.js
@@ -1,16 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require("mysql");   // mysql 모듈 require
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// 커넥션 연결
+let client = mysql.createConnection({
+  host:   '127.0.0.1',
+  user: "root",
+  password: "wjs970",
+  database: "mysqltest"
+})
+router.get('/create', function(req, res, next) {
+  client.query("SELECT * FROM products;", function(err, result, fields){
+    if(err){
+      console.log(err);
+      console.log("쿼리문에 오류가 있습니다.");
+    }
+    else{
+      res.render('create', {
+        results: result
+      });
+    }
+  });
 });
 
-// depth 경로 설정
-// router.get('/', function(req, res, next) {
-//   res.render('product/edit', {
-//     title: "Express"
-//   });
-// });
+router.post('/create', function(req, res, next) {
+  var body = req.body;
+
+  client.query("INSERT INTO products (name, modelnumber, series) VALUES (?, ?, ?)", [
+    body.name, body.modelnumber, body.series
+  ], function(){
+    res.redirect("/create");
+  });
+});
 
 module.exports = router;
