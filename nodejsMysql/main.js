201bbd82f4cf89fc34649df0e9e7a3a4eedb9373
unknown <tw450497@gmail.com>
2021-08-30 23:28:29 +0900

    nodejs & mysql 글생성기능구현

--- nodejsMysql/main.js
+++ nodejsMysql/main.js
@@ -88,22 +88,25 @@ var app = http.createServer(function(request,response){
         });
       }
     } else if(pathname === '/create'){
-      fs.readdir('./data', function(error, filelist){
-        var title = 'WEB - create';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list, `
-          <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-        `, '');
+      db.query('SELECT * FROM topic', function(error, topics){
+        console.log(topics);
+        var title = 'create';
+        var list = template.list(topics);
+        var html = template.HTML(title, list,
+            `<form action="/create_process" method="post">
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p>
+                <textarea name="description" placeholder="description"></textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>`,
+            `<a href="/create">create</a>`
+        );
         response.writeHead(200);
         response.end(html);
+
       });
     } else if(pathname === '/create_process'){
       var body = '';
@@ -112,12 +115,17 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var title = post.title;
-          var description = post.description;
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
-            response.end();
-          })
+          db.query('INSERT INTO topic (title, description, created, author_id) VALUES(?, ?, NOW(), ?)',
+            [post.title, post.description, 1],
+            function(error, result) {
+              if(error) {
+                throw error;
+              }
+              response.writeHead(302, {Location: `/?id=${result.insertId}`});
+              response.end();
+            }
+          
+          )
       });
     } else if(pathname === '/update'){
       fs.readdir('./data', function(error, filelist){
