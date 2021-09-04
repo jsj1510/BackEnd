212907431b6241625378f07bd3607cf3752747f4
unknown <tw450497@gmail.com>
2021-09-04 23:05:49 +0900

    express 페이지 삭제기능

--- express/main.js
+++ express/main.js
@@ -36,7 +36,7 @@ app.get('/page/:pageId', function(request, response) {
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
           <a href="/update/${sanitizedTitle}">update</a>
-          <form action="delete_process" method="post">
+          <form action="/delete_process" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`
@@ -84,9 +84,9 @@ app.post('/create_process', function(request, response) {
 
 
 app.get('/update/:pageId', function(request, response) {
-  fs.readdir('./data', function(error, filelist){
+  fs.readdir('./data', function(error, filelist) {
     var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
       var title = request.params.pageId;
       var list = template.list(filelist);
       var html = template.HTML(title, list,
@@ -111,24 +111,37 @@ app.get('/update/:pageId', function(request, response) {
 
 app.post('/update_process', function(request, response) {
   var body = '';
-  request.on('data', function(data){
+  request.on('data', function(data) {
       body = body + data;
   });
-  request.on('end', function(){
+  request.on('end', function() {
       var post = qs.parse(body);
       var id = post.id;
       var title = post.title;
       var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function(error){
+      fs.rename(`data/${id}`, `data/${title}`, function(error) {
         fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.writeHead(302, {Location: `/?id=${title}`});
-          response.end();
+          response.redirect(`/?id=${title}`);
         })
       });
   });
 
 });
 
+app.post('/delete_process', function(request, response) {
+  var body = '';
+  request.on('data', function(data) {
+      body = body + data;
+  });
+  request.on('end', function(){
+      var post = qs.parse(body);
+      var id = post.id;
+      var filteredId = path.parse(id).base;
+      fs.unlink(`data/${filteredId}`, function(error) {
+        response.redirect(`/`);
+      });
+  });
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
@@ -190,23 +203,7 @@ var app = http.createServer(function(request,response){
           });
       });
     } else if(pathname === '/delete_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var filteredId = path.parse(id).base;
-          fs.unlink(`data/${filteredId}`, function(error){
-            response.writeHead(302, {Location: `/`});
-            response.end();
-          })
-      });
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
+     
 });
 app.listen(3000);
 */
