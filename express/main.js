4134f0c23ca98d56da3c2e33bcf089deb3d998fa
unknown <tw450497@gmail.com>
2021-09-04 22:27:49 +0900

    express 홈페이지 루트 구현

--- express/main.js
+++ express/main.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
+var template = require('./lib/template.js');
  
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(request, response) { 
+  fs.readdir('./data', function(error, filelist){
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    ); 
+    response.send(html);
+  });
+});
  
 app.get('/page', (req, res) => res.send('/page'))
 
